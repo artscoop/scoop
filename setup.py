74a024bba2ee01d9ceb58dd352fe3bbd6ee2723a
steve <steve.kossouho@gmail.com>
2015-04-15 17:08:25 +0200

    Added dependencies.

--- setup.py
+++ setup.py
@@ -14,5 +14,22 @@ setup(
     license='',
     author='Steve Kossouho',
     author_email='steve.kossouho@gmail.com',
-    description='The django scoop project by artscoop'
+    description='The django scoop project by artscoop',
+    requires=['django', 'bleach', 'beautifulsoup', 'beautifulsoup4', 'bpython', 'coverage', 'django_autoslug', 'django_celery', 'django_jsonresponse', 'django_simple_captcha',
+              'django_static_sitemaps', 'django_translatable', 'dj_cmd', 'dnspython', 'fuzzywuzzy', 'gunicorn', 'html5lib', 'ipy', 'ipython', 'isort', 'loremipsum', 'lxml', 'markdown',
+              'mysql_python', 'ngram', 'nltk', 'numpy', 'paramiko', 'pexpect', 'pillow', 'psycopg2', 'pyproj', 'python_levenshtein', 'python_dateutil', 'python_magic', 'pytz', 'requests',
+              'simplejson', 'unicodecsv', 'unidecode'],
+    dependency_links=['https://bitbucket.org/ubernostrum/webcolors/get/default.zip',
+                      'https://github.com/giampaolo/psutil/archive/master.zip',
+                      'https://github.com/coleifer/micawber/archive/master.zip',
+                      'https://github.com/SmileyChris/easy-thumbnails/archive/master.zip',
+                      'https://github.com/jsocol/django-waffle/archive/master.zip',
+                      'https://bitbucket.org/basaundi/django-languages/get/default.zip',
+                      'https://github.com/blturner/django_inlines/archive/master.zip',
+                      'https://github.com/alex/django-filter/archive/develop.zip',
+                      'https://github.com/django-extensions/django-extensions/archive/master.zip',
+                      'https://github.com/artscoop/django-disposable-email-checker/archive/master.zip',
+                      'https://bitbucket.org/mjs7231/django-dbbackup/get/default.zip',
+                      'https://github.com/SmileyChris/django-countries/archive/master.zip',
+                      ]
 )
