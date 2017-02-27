052a0da7cb6bf335a2124323f2ca47eca10f672e
Kossouho <steve.kossouho@gmail.com>
2017-02-27 21:06:01 +0100

    Rendre l'arborescence des menus de niveau supérieur dynamique
    [+] menus.elements.menu.get_children : renvoie les éléments enfants
    du menu. Par défaut, renvoie le contenu défini dans Menu.children, mais
    peut être redéfini pour renvoyer une combinaison particulière
    d'éléments enfants selon le contexte (request)

--- scoop/menus/elements/menu.py
+++ scoop/menus/elements/menu.py
@@ -34,9 +34,21 @@ class Menu(object):
             if isinstance(item, Item):
                 self.children.append(item)
 
+    # Getter
+    def get_children(self, request):
+        """
+        Renvoyer les éléments enfants du menu
+
+        Peut être overridé pour renvoyer une liste d'éléments enfants
+        dynamique.
+
+        :param request: requête HTTP
+        """
+        return self.children
+
     # Rendu
     def render(self, request, style=None):
         """ Rendre le menu """
-        outputs = [child.render(request, style=self.style) for child in self.children]
+        outputs = [child.render(request, style=self.style) for child in self.get_children(request)]
         data = {'children': outputs, 'menu': self}
         return render_to_string('menus/menu-menu-{style}.html'.format(style=self.style or 'nav'), data, request)
