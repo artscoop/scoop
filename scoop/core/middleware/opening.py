a19d6960a9debc0e1ad8929442d8ec63dca0e4c0
Steve Kossouho <steve.kossouho@gmail.com>
2017-05-24 07:02:49 +0200

    Corriger OpeningHoursMiddleware
    [f] Correction de la détection d'ouverture du site; ne fonctionnait pas
    après minuit si l'heure de fermeture excédait minuit.

--- scoop/core/middleware/opening.py
+++ scoop/core/middleware/opening.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 import logging
-
 from django.conf import settings
 from django.utils import timezone
 
 from scoop.core.util.django.middleware import MiddlewareBase
 from scoop.core.util.django.templates import do_render
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,13 +16,16 @@ class OpeningHoursMiddleware(MiddlewareBase):
     # Définir les horaires d'ouverture du site
     frames = getattr(settings, "OPENING_HOURS", [(0, 24)])
     groups = getattr(settings, "OPENING_HOURS_GROUPS_EXCLUDE", ["VIP"])
+    template = getattr(settings, "OPENING_HOURS_TEMPLATE", 'opening-hours.html')
 
     def now_in_timeframes(self, timeframes):
         """ Renvoyer si l'heure est contenue dans une des plages horaires d'ouverture """
         hour = timezone.now().hour
         for timeframe in timeframes:
-            if timeframe[0] <= hour < timeframe[1]:
-                return True
+            if timeframe[0] < timeframe[1]:
+                return timeframe[0] <= hour < timeframe[1]
+            elif timeframe[0] > timeframe[1]:
+                return hour < timeframe[1] or hour >= timeframe[0]
         return False
 
     def _call__(self, request):
@@ -34,6 +37,6 @@ class OpeningHoursMiddleware(MiddlewareBase):
             if not self.now_in_timeframes(OpeningHoursMiddleware.frames):
                 if not user.is_staff and (self.groups and not user.groups.filter(name__in=self.groups).exists()):
                     # Changer le contenu de la page
-                    return do_render(request, template="page/project/restricted/opening-hours.html", data={'hours': self.frames}, status_code=503,
+                    return do_render(request, template=OpeningHoursMiddleware.template, data={'hours': self.frames}, status_code=503,
                                      use_request=False)
         return response
